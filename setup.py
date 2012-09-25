5bad6cc09a20e734769edf4ad15c9d588384bd96
Rémy Léone <remy.leone@gmail.com>
2012-09-26 00:29:44 +0200

    Corrections

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author='Rémy Léone',
     license=open('LICENCE').read(),
     author_email='remy.leone@gmail.com',
-    long_description = open('README.md').read() + '\n\n',
-    url = "http://pycolo.sieben.fr",
+    long_description=open('README.md').read() + '\n\n',
+    url="http://pycolo.sieben.fr",
 )
