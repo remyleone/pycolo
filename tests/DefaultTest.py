9f49e8cea68707fb1174a936bdef2f1f70cf3d31
Rémy Léone <remy.leone@gmail.com>
2012-10-05 00:50:23 +0200

    Corrections des erreurs.

--- tests/DefaultTest.py
+++ tests/DefaultTest.py
@@ -1,174 +1,135 @@
 # coding=utf-8
 
 from pycolo import Resource
+from pycolo.codes import codes
+from pycolo import Response
+from pycolo.codes import mediaCodes
 
-/**
- * This resource implements a test of specification for the
- * ETSI IoT CoAP Plugtests, Paris, France, 24 - 25 March 2012.
- * 
- * @author Matthias Kovatsch
- */
-class DefaultTest(Resource) {
 
-	public DefaultTest() {
-		super("test");
-		setTitle("Default test resource");
-	}
+class DefaultTest(Resource):
+    """
+    This resource implements a test of specification for the
+    ETSI IoT CoAP Plugtests, Paris, France, 24 - 25 March 2012.
+    """
 
-	@Override
-	public void performGET(GETRequest request) {
+    def __int__(self):
 
-		// Check: Type, Code
+        self.title = "Default test resource"
 
-		// create response
-		Response response = new Response(CodeRegistry.RESP_CONTENT);
-		
-		StringBuilder payload = new StringBuilder();
-		
-		payload.append(String.format("Type: %d (%s)\nCode: %d (%s)\nMID: %d",
-									 request.getType().ordinal(),
-									 request.typeString(),
-									 request.getCode(),
-									 CodeRegistry.toString(request.getCode()),
-									 request.getMID()
-									));
-		
-		if (request.getToken().length>0) {
-			payload.append("\nToken: ");
-			payload.append(request.getTokenString());
-		}
-		
-		if (payload.length()>64) {
-			payload.delete(62, payload.length());
-			payload.append('>>');
-		}
-		
-		// set payload
-		response.setPayload(payload.toString());
-		response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
-		
-		// complete the request
-		request.respond(response);
-	}
+    def performGET(request):
 
-	@Override
-	public void performPOST(POSTRequest request) {
-		
-		// Check: Type, Code, has Content-Type
+        # Check: Type, Code
 
-		// create new response
-		Response response = new Response(CodeRegistry.RESP_CREATED);
-		
-		/*
-		StringBuilder payload = new StringBuilder();
-		
-		payload.append(String.format("Type: %d (%s)\nCode: %d (%s)\nMID: %d",
-									 request.getType().ordinal(),
-									 request.typeString(),
-									 request.getCode(),
-									 CodeRegistry.toString(request.getCode()),
-									 request.getMID()
-									));
-		payload.append(String.format("\nCT: %d\nPL: %d",
-				  request.getContentType(),
-				  request.payloadSize()
-				));
-		
-		if (request.getToken().length>0) {
-			payload.append("\nTo: ");
-			payload.append(request.getTokenString());
-		}
-		
-		if (payload.length()>64) {
-			payload.delete(62, payload.length());
-			payload.append('>>');
-		}
-		
-		response.setPayload(payload.toString());
-		response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
-		*/
-		
-		response.setLocationPath("/nirvana");
+        # create response
+        response = Response(codes.RESP_CONTENT)
 
-		// complete the request
-		request.respond(response);
-	}
+        payload =  "Type: %d (%s)\nCode: %d (%s)\nMID: %d" %\
+                                     request.getType().ordinal(),\
+                                     request.typeString(),\
+                                     request.code,\
+                                     request.MID
 
-	@Override
-	public void performPUT(PUTRequest request) {
-		
-		// Check: Type, Code, has Content-Type
+        if request.getToken().length > 0:
+            payload.append("Token: ")
+            payload.append(request.getTokenString())
 
-		// create new response
-		Response response = new Response(CodeRegistry.RESP_CHANGED);
-		
-		/*
-		StringBuilder payload = new StringBuilder();
-		
-		payload.append(String.format("Type: %d (%s)\nCode: %d (%s)\nMID: %d",
-									 request.getType().ordinal(),
-									 request.typeString(),
-									 request.getCode(),
-									 CodeRegistry.toString(request.getCode()),
-									 request.getMID()
-									));
-		payload.append(String.format("\nCT: %d\nPL: %d",
-				  request.getContentType(),
-				  request.payloadSize()
-				));
-		
-		if (request.getToken().length>0) {
-			payload.append("\nTo: ");
-			payload.append(request.getTokenString());
-		}
-		
-		if (payload.length()>64) {
-			payload.delete(62, payload.length());
-			payload.append('>>');
-		}
-		
-		response.setPayload(payload.toString());
-		response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
-		*/
+        if len(payload) > 64:
+            payload.delete(62, len(payload))
+            payload.append('>>')
 
-		// complete the request
-		request.respond(response);
-	}
 
-	@Override
-	public void performDELETE(DELETERequest request) {
-		
-		// Check: Type, Code, has Content-Type
+        # set payload
+        response.payload = payload
+        response.contentType = mediaCodes.text
 
-		// create new response
-		Response response = new Response(CodeRegistry.RESP_DELETED);
-		
-		/*
-		StringBuilder payload = new StringBuilder();
-		
-		payload.append(String.format("Type: %d (%s)\nCode: %d (%s)\nMID: %d",
-									 request.getType().ordinal(),
-									 request.typeString(),
-									 request.getCode(),
-									 CodeRegistry.toString(request.getCode()),
-									 request.getMID()
-									));
-		
-		if (request.getToken().length>0) {
-			payload.append("\nToken: ");
-			payload.append(request.getTokenString());
-		}
-		
-		if (payload.length()>64) {
-			payload.delete(62, payload.length());
-			payload.append('>>');
-		}
-		
-		response.setPayload(payload.toString());
-		response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
-		*/
+        # complete the request
+        request.respond(response)
 
-		// complete the request
-		request.respond(response);
-	}
-}
+    def performPOST(request):
+
+        # Check: Type, Code, has Content-Type
+
+        # create new response
+        response = Response(codes.RESP_CREATED)
+
+        payload = "Type: %d (%s)\nCode: %d (%s) MID: %d" %\
+                                     request.type.ordinal(),\
+                                     request.type,\
+                                     request.code,\
+                                     request.MID
+
+        payload.join("\nCT: %d\nPL: %d" % request.getContentType(), request.payloadSize())
+
+        if request.getToken():
+            payload.append("\nTo: ")
+            payload.append(request.getTokenString())
+
+        if len(payload) > 64:
+            payload.delete(62, payload.length())
+            payload.append('>>')
+
+        response.payload = str(payload)
+        response.contentType = mediaCodes.TEXT_PLAIN
+
+        response.setLocationPath("/nirvana")
+
+        # complete the request
+        request.respond(response)
+
+    def performPUT(request):
+
+        # Check: Type, Code, has Content-Type
+
+        # create new response
+        response = Response(codes.RESP_CHANGED)
+
+        payload = "Type: %d (%s)\nCode: %d (%s)\nMID: %d" %\
+                                     request.getType().ordinal(),\
+                                     request.typeString(),\
+                                     request.getCode(),\
+                                     request.getMID()
+
+        payload.append("\nCT: %d\nPL: %d" % request.getContentType(), request.payloadSize())
+
+        if request.getToken().length > 0:
+            payload.append("\nTo: ")
+            payload.append(request.getTokenString())
+
+        if payload.length() > 64:
+            payload.delete(62, payload.length())
+            payload.append('>>')
+
+        response.payload = str(payload)
+        response.contentType = mediaCodes.text
+
+
+        # complete the request
+        request.respond(response)
+
+
+    def performDELETE(request):
+
+        # Check: Type, Code, has Content-Type
+
+        # create new response
+        response = Response(codes.RESP_DELETED);
+
+        payload = "Type: %d (%s)\nCode: %d (%s)\nMID: %d" %\
+                                     request.getType().ordinal(),\
+                                     request.typeString(),\
+                                     request.getCode(),\
+                                     request.getMID()
+
+        if request.getToken().length > 0:
+            payload.append("\nToken: ")
+            payload.append(request.getTokenString())
+
+        if payload.length() > 64:
+            payload.delete(62, payload.length())
+            payload.append('>>')
+
+        response.setPayload(payload.toString())
+        response.setContentType(mediaCodes.TEXT_PLAIN)
+
+        # complete the request
+        request.respond(response)
