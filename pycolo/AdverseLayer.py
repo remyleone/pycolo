f80ea97ddf97ff3c16d76078aac298b7080d673d
Rémy Léone <remy.leone@gmail.com>
2012-10-08 18:42:20 +0200

    Corrections et paramètres par défaut

--- pycolo/AdverseLayer.py
+++ pycolo/AdverseLayer.py
@@ -11,30 +11,18 @@ class AdverseLayer(UpperLayer):
     MessageLayer and UDPLayer etc.
     """
 
-    def __init__(self, txPacketLossProbability, rxPacketLossProbability):
-        """ generated source for method __init__ """
-        super(AdverseLayer, self).__init__()
+    def __init__(self, txPacketLossProbability=0.0, rxPacketLossProbability=0.0):
         self.txPacketLossProbability = txPacketLossProbability
         self.rxPacketLossProbability = rxPacketLossProbability
 
-    def __init___0(self):
-        """ generated source for method __init___0 """
-        super(AdverseLayer, self).__init__()
-        self.__init__(0.01, 0.00)
-
     def doSendMessage(self, msg):
-        """ generated source for method doSendMessage """
         if random.SystemRandom() >= self.txPacketLossProbability:
             self.sendMessageOverLowerLayer(msg)
         else:
             logging.info("[%s] Outgoing message dropped: %s\n" % self.getClass().__name__, msg.key())
 
     def doReceiveMessage(self, msg):
-        """ generated source for method doReceiveMessage """
         if random.SystemRandom() >= self.rxPacketLossProbability:
             self.deliverMessage(msg)
         else:
             logging.info("[%s] Incoming message dropped: %s\n", self.getClass().__name__, msg.key())
-
-    txPacketLossProbability = float()
-    rxPacketLossProbability = float()
