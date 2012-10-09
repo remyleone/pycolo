938060785d73d14d99a8a5379358e36f07fa72fc
Rémy Léone <remy.leone@gmail.com>
2012-10-09 17:46:44 +0200

    Correction

--- tests/TestImageResource.py
+++ tests/TestImageResource.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 
-import logging
-from pycolo import Response, Resource, codes
+from pycolo import resource
+from pycolo.codes import mediaCodes, codes
+from pycolo.message import Response
 
 
-class ImageResource(Resource):
+class ImageResource(resource):
     """
     This class implements an "/image" resource for demonstration purposes.
 
@@ -16,16 +17,8 @@ class ImageResource(Resource):
     supported = list()
 
     def __init__(self):
-        """ generated source for method __init__ """
-        super(ImageResource, self).__init__()
-        self.__init__("image")
-
-    @__init__.register(object, str)
-    def __init___0(self, resourceIdentifier):
-        """ generated source for method __init___0 """
-        super(ImageResource, self).__init__(resourceIdentifier)
         self.title = "GET an image with different content-types"
-        self.setResourceType("Image")
+        self.resourceType = "Image"
         self.supported.append(codes.mediaCodes["IMAGE_PNG"])
         self.supported.append(codes.mediaCodes["IMAGE_JPEG"])
         self.supported.append(codes.mediaCodes["IMAGE_GIF"])
@@ -35,39 +28,17 @@ class ImageResource(Resource):
         self.maximumSizeEstimate = 18029
         self.observable = False
 
-    #  REST Operations /////////////////////////////////////////////////////////
     def performGET(self, request):
-        """ generated source for method performGET
+
+        """
+        Give back a image in a binary form.
         :param request:
         """
-        filename = "data/image/"
-        ct = mediaTypeRegistry.IMAGE_PNG
-        #  content negotiation
-        if (ct=MediaTypeRegistry.contentNegotiation(ct, self.supported, request.getOptions(OptionNumberRegistry.ACCEPT))) == MediaTypeRegistry.UNDEFINED:
-            request.respond(codes.RESP_NOT_ACCEPTABLE, "Accept GIF, JPEG, PNG, or TIFF")
-            return
-        filename += "image." + MediaTypeRegistry.toFileExtension(ct)
-        # load representation from file
-        file_ = File(filename)
-        if not file_.exists():
-            request.respond(codes.RESP_INTERNAL_SERVER_ERROR, "Representation not found")
-            return
-        # get length of file
-        fileLength = int(len(file_))
-        fileIn = None
-        fileData = [None] * fileLength
-        try:
-            # open input stream from file
-            fileIn = FileInputStream(file_)
-            # read file into byte array
-            fileIn.read(fileData)
-            fileIn.close()
-        except Exception as e:
-            request.respond(codes.RESP_INTERNAL_SERVER_ERROR, "IO error")
-            logging.critical("/image IO error: " + e.getMessage())
-            return
-        #  create response
+        ct = mediaCodes.IMAGE_PNG
+        fileData = bytearray()  # load representation from file
+        with open("img/python.png") as f:
+            fileData = f.read()
         response = Response(codes.RESP_CONTENT)
         response.payload = fileData
-        response.setContentType(ct)  # set content type
+        response.contentType = ct  # set content type
         request.respond(response)  # complete the request
