9344d6943af9d3fc8f24ef9a9c93153c69726778
Rémy Léone <remy.leone@gmail.com>
2012-11-07 16:39:25 +0100

    Correction

--- tests/TestBytes.py
+++ tests/TestBytes.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from random import random
 
 import unittest
 import math
@@ -6,48 +7,72 @@ import math
 class TestBytes(unittest.TestCase):
 
     def test32BitInt(self):
-        intIn = 0x87654321
+        """
+        Test random 32 bit integer
+        """
+        intIn = random.randint(0, 0xFFFFFFFF)
         temp = intIn.to_bytes(32, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test32BitIntZero(self):
+        """
+        Test min 32 bit integer
+        """
         intIn = int()
         temp = intIn.to_bytes(32, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test32BitIntOne(self):
+        """
+        Test max 32 bits integer
+        """
         intIn = 0xFFFFFFFF
         temp = intIn.to_bytes(32, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test16BitInt(self):
+        """
+        Test 16 bits integer
+        """
         intIn = 0x00004321
         temp = intIn.to_bytes(16, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test8BitInt(self):
+        """
+        Test 8 bits integer
+        """
         intIn = 0x00000021
         temp = intIn.to_bytes(8, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test4BitInt(self):
+        """
+        Test 4 bits integer
+        """
         intIn = 0x0000005
         temp = intIn.to_bytes(4, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test2BitInt(self):
+        """
+        Test 2 bits integer
+        """
         intIn = 0x00000002
         temp = intIn.to_bytes(2, 'little')
         intOut = int.from_bytes(temp, 'little')
         self.assertEquals(intIn, intOut)
 
     def test1BitInt(self):
+        """
+        Test 1 bit integer
+        """
         intIn = 0x00000001
         temp = intIn.to_bytes(1, 'little')
         intOut = int.from_bytes(temp, 'little')
