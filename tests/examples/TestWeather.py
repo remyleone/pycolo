06458b0680c9bfb431267fd4651389a28d7b903e
Rémy Léone <remy.leone@gmail.com>
2012-12-05 14:41:32 +0100

    Skeleton for a HTTP/CoAP simple app

--- tests/examples/TestWeather.py
+++ tests/examples/TestWeather.py
@@ -1,70 +1,32 @@
 # coding=utf-8
 
 """
-TODO
+This test implement mix of HTTP request to a JSON API and CoAP server
+serving this information.
 """
 
-from pycolo.resource import Resource
+from urllib.request import urlopen
+import unittest
+from pycolo import Endpoint
+from pycolo.request import request as coap
 
-class ParisWeatherResource(Resource):
-    """
-    Defines a resource that returns the current weather on a GET request.
-    """
+class TestWebCoAP(unittest.TestSuite):
 
-    def __init__(self):
-        """
-        Constructor for a new ParisWeatherResource
-        """
+    def setUp():
+        server = Endpoint(__name__)
 
-    #        super(ZurichWeatherResource, self).__init__("weatherResource")
-    #        self.title = "GET the current weather in Paris"
-    #        self.setResourceType("ZurichWeather")
-    #        self.observable = True
-    #        self.supported.add(mediaCodes.text)
-    #        self.supported.add(mediaCodes.json)
-    #        for ct in supported:
-    #            self.setContentTypeCode(ct)
-    #        self.getParisWeather()
-    #        #  Set timer task scheduling
-    #        timer = Timer()
-    #        timer.schedule(ParisWeatherTask(), 0, self.WEATHER_MAX_AGE)
+        @server.route("/meteo",
+                    observable=True, t=["json", "text"], repeat=5,
+                    title="GET the current weather in Paris")
+        def meteo(self):
+            r = urlopen("http://openweather.com/")
+            return r.readall()
 
-    class ParisWeatherTask:
-        """
-        TODO
-        """
+    def testSimpleObserve(self):
+        meteo = coap.get(self.server.url + "/meteo")
+        temp = meteo
+        self.assertTrue(-50 < temp < 50)
 
-        def run(self):
-            """
-            TODO
-            """
-            pass
 
-        #            weatherOLD = str(self.weatherXML)
-        #            self.getParisWeather()
-        #            if not weatherOLD == self.weatherXML:
-        #                changed()
-
-        def getParisWeather(self):
-            """
-            TODO
-            """
-            pass
-
-        def performGET(self, request):
-            """
-            TODO
-            :param request:
-            """
-            #        ct = mediaCodes.text
-            #        if ct=MediaTypeRegistry.contentNegotiation(ct, self.supported, request.getOptions(OptionNumberRegistry.ACCEPT)) == MediaTypeRegistry.UNDEFINED:
-            #        request.respond(codes.RESP_NOT_ACCEPTABLE, "Supports text/plain and application/xml")
-            #        return
-            #    response = Response(codes.RESP_CONTENT)
-            #    response.setMaxAge(self.WEATHER_MAX_AGE / 1000)
-            #    if ct == MediaTypeRegistry.APPLICATION_XML:
-            #        response.payload = self.weatherXML, mediaCodes.APPLICATION_XML
-            #    else:
-            #        response.payload = self.weatherPLAIN, mediaCodes.text
-            #    request.respond(response)
-            #
+if __name__ == '__main__':
+    unittest.main()
