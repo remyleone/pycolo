52aabf8af16e54a2b0a2795dffd8a7c006edeb3d
Rémy Léone <remy.leone@gmail.com>
2013-01-24 17:23:02 +0100

    Schema de base

--- tests/examples/TestWeather.py
+++ tests/examples/TestWeather.py
@@ -7,25 +7,55 @@ serving this information.
 
 from urllib.request import urlopen
 import unittest
-from pycolo import Endpoint
-from pycolo.request import request as coap
+from pycolo.endpoint import Endpoint
+import pycolo as coap
+from pycolo.observe import redis_observer
 
-class TestWebCoAP(unittest.TestSuite):
+publish = redis_observer.publish
+subscribe = redis_observer.subscribe
 
-    def setUp():
-        server = Endpoint(__name__)
 
-        @server.route("/meteo",
-                    observable=True, t=["json", "text"], repeat=5,
-                    title="GET the current weather in Paris")
-        def meteo(self):
-            r = urlopen("http://openweather.com/")
-            return r.readall()
+class TestWebCoAP(unittest.TestCase):
+    """
+    Interface CoAP, HTTP and observing in a simple weather setting.
+    """
 
-    def testSimpleObserve(self):
-        meteo = coap.get(self.server.url + "/meteo")
-        temp = meteo
+    def setUp(self):
+        """
+        Set up a loop to fetch the weather.
+        :return:
+        """
+        self.server = Endpoint(__name__)
+
+        def weather(self):
+            """
+            Infinite loop checking weather on a exterior website
+            """
+            while True:
+                r = urlopen("http://openweather.com/")
+                publish(content=r.readall())
+
+        self.server.add_url_rule("/weather",
+                              function=weather,
+                              observable=True,
+                              t=["json", "text"],
+                              title="GET the current weather in Paris")
+
+    def test_simple_get(self):
+        """
+        Simple get
+        """
+        temp = coap.get(self.server.url + "/weather")
         self.assertTrue(-50 < temp < 50)
+
+    def test_simple_observe(self):
+        """
+        Simple observe
+        """
+        obs = coap.observe(self.server.url + "/weather",
+                           sub=subscribe)
+        for item in obs.listen(5):
+            self.assertTrue(-50 < item < 50)
 
 
 if __name__ == '__main__':
